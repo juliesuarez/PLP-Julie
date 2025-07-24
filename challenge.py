82acb28502f04a32f1306154c04cd45b9742c5dc
Julie Nakawesi <julietcristiana780@gmail.com>
2025-07-24 19:14:11 +0300

    practice

--- challenge.py
+++ challenge.py
@@ -2,4 +2,4 @@ name = 'Enter your name here:'
 name = input(name)  # This line prompts the user to enter their name.
 favorite_color = input('What is your favorite color? ')  # This line prompts the user to input their favorite color.
 print('Hello', name ,'Your favorite color is', favorite_color)  # This line prints a greeting message with the user's name.
-name ='python'🐍
+name ='python'
